a5edb24e95e92922678fb39f94b4e2928b7f80cc
Medon Abraham <medon@boskalis-guests.com>
2025-11-19 12:23:46 +0100

    A1-F1; Integrate lib-version in app + updated README (F1)

--- src/main/java/frontend/Main.java
+++ src/main/java/frontend/Main.java
@@ -2,11 +2,13 @@ package frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.doda25.team9.libversion.VersionUtil; 
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Using lib-version: " + VersionUtil.getVersion());
         SpringApplication.run(Main.class, args);
     }
 
