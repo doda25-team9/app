edd2a312ee249191b4a86af081e73369f6b2c26a
Zofia Rogacka-Trojak <z.rogacka-trojak@student.tudelft.nl>
2025-12-18 16:32:42 +0100

    Additional metrics to present Gauge metric

--- src/main/java/frontend/ctrl/FrontendController.java
+++ src/main/java/frontend/ctrl/FrontendController.java
@@ -63,6 +63,10 @@ public class FrontendController {
                 new Counter( "predictions_result_total", "Total number of SMS predictions with result"));
         metricsRegistry.addHistogram("sms_length", new Histogram(List.of(10.0, 20.0, 30.0, 40.0, 50.0, 100.0, 200.0, 500.0),
                 "sms_length", "Histogram of SMS lengths in characters"));
+        metricsRegistry.addGauge("last_request_duration_ms",
+                new Gauge("last_request_duration_ms", "Duration of the last request in milliseconds"));
+        metricsRegistry.addGauge("last_sms_length_characters",
+                new Gauge("last_sms_length_characters", "Length of the last SMS in characters"));
     }
 
 
@@ -82,7 +86,7 @@ public class FrontendController {
     @ResponseBody
     public Sms predict(@RequestBody Sms sms) {
         System.out.printf("Requesting prediction for \"%s\" ...\n", sms.sms);
-        recordRequestMetrics();
+        recordRequestMetrics(sms);
 
         long startTime = System.currentTimeMillis();
         sms.result = getPrediction(sms);
@@ -105,14 +109,16 @@ public class FrontendController {
         }
     }
 
-    private void recordRequestMetrics() {
+    private void recordRequestMetrics(Sms sms) {
         metricsRegistry.getCounter("sms_requests_total").increment("endpoint", "/sms");
         metricsRegistry.getGauge("active_users").increment("endpoint", "/sms");
+        metricsRegistry.getGauge("last_sms_length_characters").set("endpoint", "/sms", sms.sms.length());
 
     }
 
     private void recordPredictionMetrics(long durationMs) {
         metricsRegistry.getHistogram("request_duration").record("endpoint", "/sms", durationMs / 1000.0);
+        metricsRegistry.getGauge("last_request_duration_ms").set("endpoint", "/sms", (int) durationMs);
     }
 
     private void recordResultMetrics(Sms sms) {
